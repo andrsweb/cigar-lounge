b7c627b4bc028a12876e359f78225dc3b76e48d2
webandrs <d3vv.proo@gmail.com>
2025-09-09 09:30:43 +0700

    feat: created scrollToTop func

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict'
+
+	scrollToTop();
 })
+
+const scrollToTop = () => {
+	const scrollButton = document.querySelector('.scroll-top');
+
+	if(!scrollButton) return;
+
+	scrollButton.addEventListener('click', () => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth'
+		})
+	})
+}
