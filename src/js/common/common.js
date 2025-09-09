07b5c52105219dd761bceceabe1def1270886487
webandrs <d3vv.proo@gmail.com>
2025-09-09 10:42:17 +0700

    feat: created burger menu

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -1,8 +1,38 @@
+import {enableBodyScroll, disableBodyScroll} from "body-scroll-lock";
+
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict'
 
+	toggleMenu();
 	scrollToTop();
 })
+
+const toggleMenu = () => {
+	const header = document.querySelector('.header');
+	const burgerButton = header?.querySelector('.burger-button');
+	const targetEl = header?.querySelector('#burger-menu');
+
+	header.classList.add('is-hidden');
+
+	if (!burgerButton || !header) return;
+
+	burgerButton.addEventListener('click', () => {
+		if (header.classList.contains('is-active')) {
+			header.classList.remove('is-active');
+			header.classList.add('is-hiding');
+
+			setTimeout(() => {
+				header.classList.remove('is-hiding');
+				header.classList.add('is-hidden');
+				enableBodyScroll(targetEl);
+			}, 300);
+		} else {
+			header.classList.remove('is-hidden');
+			header.classList.add('is-active');
+			disableBodyScroll(targetEl);
+		}
+	});
+};
 
 const scrollToTop = () => {
 	const scrollButton = document.querySelector('.scroll-top');
